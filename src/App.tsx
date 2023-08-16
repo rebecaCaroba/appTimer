c27a246b5213cd5b71f3fcbc72847de8e5aaaaf2
rebecaCaroba <rebecacaroba@gmail.com>
2023-08-16 09:17:27 -0300

    React Router DOM

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,15 @@
-import { Button } from './components/Button'
 import { ThemeProvider } from 'styled-components'
 import { defaultThemes } from './styles/themes/default'
 import { GlobalStyle } from './styles/global'
+import { BrowserRouter } from 'react-router-dom'
+import { Router } from './Router'
 
 function App() {
   return (
     <ThemeProvider theme={defaultThemes}>
-      <Button varient="primary" />
-      <Button varient="secondary" />
-      <Button varient="danger" />
-      <Button varient="success" />
-      <Button />
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
       <GlobalStyle />
     </ThemeProvider>
   )
--- src/components/Button.tsx
+++
@@ -1,9 +0,0 @@
-import { ButtonContainer, ButtonVariant } from './Button.styles'
-
-interface ButtonProps {
-  varient?: ButtonVariant
-}
-
-export function Button({ varient = 'primary' }: ButtonProps) {
-  return <ButtonContainer varient={varient}>Enviar</ButtonContainer>
-}
