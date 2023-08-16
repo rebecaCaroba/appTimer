b2cd9968b777e4182c66919a84f7aeb9532e7441
rebecaCaroba <rebecacaroba@gmail.com>
2023-08-16 08:29:13 -0300

    Cores e Fontes

--- src/components/Button.styles.ts
+++ src/components/Button.styles.ts
@@ -20,7 +20,7 @@ export const ButtonContainer = styled.button<ButtonContainerProps>`
     border: 0;
     margin: 8px;
 
-    background-color: ${props => props.theme.primary};
+    background-color: ${props => props.theme['yellow-500']};
     color: ${props => props.theme.white}
 
 
--- src/styles/themes/default.ts
+++ src/styles/themes/default.ts
@@ -1,5 +1,21 @@
 export const defaultThemes = {
     white: '#fff',
-    primary: 'rebeccapurple',
-    secondary: 'gold'
+    
+    'gray-100': '#E1E1E6',
+    'gray-300': '#C4C4CC',
+    'gray-400': '#8D8D99',
+    'gray-500': '#7C7C8A',
+    'gray-600': '#323238',
+    'gray-700': '#29292E',
+    'gray-800': '#202024',
+    'gray-900':' #121214',
+
+    'yellow-100': '#fba94c',
+    'yellow-300': '#f1b80d',
+    'yellow-500': '#FFA500',
+    'yellow-700': '#cd8d00',
+
+    'red-500': '#ab222e',
+    'red-700': '#7a1921',
+
 }
