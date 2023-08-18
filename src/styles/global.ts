595bcecde16d4b2d2c6deee6587fef32138ccc0b
rebecaCaroba <rebecacaroba@gmail.com>
2023-08-18 08:41:52 -0300

    Estilização Home

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -15,6 +15,7 @@ export const GlobalStyle = createGlobalStyle`
     body {
         background-color: ${(props) => props.theme['gray-900']};
         color: ${(props) => props.theme['gray-100']};
+        -webkit-font-smoothing: antialiased;
     }
 
     body, input-security, textarea, button {
