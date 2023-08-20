cd16e4e4e8e50a2f3729e93cb48ba83bd5d721a0
rebecaCaroba <rebecacaroba@gmail.com>
2023-08-20 17:53:20 -0300

    Criando countdown

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -33,6 +33,7 @@ interface Cycle {
 export function Home() {
   const [cycles, setCyles] = useState<Cycle[]>([])
   const [activeCyclesId, setActiveCyclesId] = useState<string | null>(null)
+  const [amountSecondsPassed, setAmountSecondsPassed] = useState(0)
 
   const { register, handleSubmit, watch, reset } = useForm<NewCycleFormData>({
     resolver: zodResolver(newCycleFormValidationSchema),
@@ -54,8 +55,17 @@ export function Home() {
     reset()
   }
 
-  const activeCycle = cycles.find((cycle) => cycle.id == activeCyclesId)
-  console.log(activeCycle)
+  const activeCycle = cycles.find((cycle) => cycle.id === activeCyclesId)
+
+  const totalSeconds = activeCycle ? activeCycle.minutesAmount * 60 : 0
+  const currentSeconds = activeCycle ? totalSeconds - amountSecondsPassed : 0
+
+  const minutesAmount = Math.floor(currentSeconds / 60)
+  const secondsAmount = currentSeconds % 60
+
+  const minutes= String(minutesAmount).padStart(2, '0')
+  const seconds = String(secondsAmount).padStart(2, '0')
+
   const task = watch('task')
   const isSubmitDisabled = !task
 
@@ -92,11 +102,11 @@ export function Home() {
         </FormContainer>
 
         <CountdownContainer>
-          <span>0</span>
-          <span>0</span>
+          <span>{minutes[0]}</span>
+          <span>{minutes[1]}</span>
           <Separator>:</Separator>
-          <span>0</span>
-          <span>0</span>
+          <span>{seconds[0]}</span>
+          <span>{seconds[1]}</span>
         </CountdownContainer>
         <StartCountdownButton disabled={isSubmitDisabled} type="submit">
           <Play size={24} />
